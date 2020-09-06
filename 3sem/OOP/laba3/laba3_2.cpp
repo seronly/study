1e37949b49722c8085153a7c9ed76921185eb3a9
SerOnly <seronly@mail.ru>
2020-09-06 14:07:35 +0700

    edit

--- 3sem/OOP/laba3/laba3_2.cpp
+++ 3sem/OOP/laba3/laba3_2.cpp
@@ -19,12 +19,28 @@ T days_left(T year, T month, T day)
 {
     return n;
 }
+// T days_left(T year, T month, T day)
+// {
+//     return n;
+// }
 
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
+
+    date data1, data2;
+
+    cout << "¬ведите год, мес€ц и день: ";
+    cin >> data1.year;
+    cin >> data1.month;
+    cin >> data1.day;
+    cout << "ƒо конца мес€ца осталось: " << days_left(data1.year, data1.month, data1.day) << " дней" << endl;
+    cout << "\n¬ведите мес€ц и день: ";
+    cin >> data2.month;
+    cin >> data2.day;
+    cout << "ƒо конца мес€ца осталось: " << days_left(2020, data2.month, data2.day) << " дней" << endl;
 
     return 0;
 }
