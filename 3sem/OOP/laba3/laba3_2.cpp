ffec55b65f3f61cb36bc3aa2aa2e99957005f733
SerOnly <seronly@mail.ru>
2020-09-06 12:00:10 +0700

    laba3 edit

---
+++ 3sem/OOP/laba3/laba3_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <Windows.h>
+#include <locale>
+#include <string>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    return 0;
+}
