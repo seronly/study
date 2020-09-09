153ae6186ba205db09cbf4d38c255d9ebf1713ee
SerOnly <seronly@mail.ru>
2020-09-09 12:00:05 +0700

    rly done

--- 3sem/OOP/laba3/laba3_1.cpp
+++ 3sem/OOP/laba3/laba3_1.cpp
@@ -12,6 +12,57 @@ struct date
     int year, month, day;
 };
 
+int days_left(date data)
+{
+    int n = 0;
+    switch (data.month)
+    {
+    case 1:
+        n = 31;
+        break;
+    case 2:
+        if (data.year % 4 == 0 && data.year % 100 != 0 || data.year % 400 == 0) // високосный год или нет
+            n = 29;
+        else
+            n = 28;
+        break;
+    case 3:
+        n = 31;
+        break;
+    case 4:
+        n = 30;
+        break;
+    case 5:
+        n = 31;
+        break;
+    case 6:
+        n = 30;
+        break;
+    case 7:
+        n = 31;
+        break;
+    case 8:
+        n = 31;
+        break;
+    case 9:
+        n = 30;
+        break;
+    case 10:
+        n = 31;
+        break;
+    case 11:
+        n = 30;
+        break;
+    case 12:
+        n = 31;
+        break;
+    default:
+        break;
+    }
+
+    return (n - data.day);
+}
+
 int days_left(int year, int month, int day)
 {
     int n = 0;
@@ -21,7 +72,7 @@ int days_left(int year, int month, int day)
         n = 31;
         break;
     case 2:
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) // високосный год или нет
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
             n = 29;
         else
             n = 28;
@@ -124,21 +175,32 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
-    date data1, data2;
+    date data;
+    int year, month, day;
+
+    cout << "Перегрузка функции для структуры: \nВведите год: ";
+    cin >> data.year;
+    cout << "Месяц: ";
+    cin >> data.month;
+    cout << "День: ";
+    cin >> data.day;
+    cout << "До конца месяца осталось: " << days_left(data) << " дней\n"
+         << endl;
 
     cout << "Перегрузка функции с 3 параметрами: \nВведите год: ";
-    cin >> data1.year;
+    cin >> year;
     cout << "Месяц: ";
-    cin >> data1.month;
+    cin >> month;
     cout << "День: ";
-    cin >> data1.day;
-    cout << "До конца месяца осталось: " << days_left(data1.year, data1.month, data1.day) << " дней\n"
+    cin >> day;
+    cout << "До конца месяца осталось: " << days_left(year, month, day) << " дней\n"
          << endl;
+
     cout << "Перегрузка функции с 2 параметрами: \nВведите месяц: ";
-    cin >> data2.month;
+    cin >> month;
     cout << "День: ";
-    cin >> data2.day;
-    cout << "До конца месяца осталось: " << days_left(data2.month, data2.day) << " дней" << endl;
+    cin >> day;
+    cout << "До конца месяца осталось: " << days_left(month, day) << " дней" << endl;
 
     return 0;
 }
