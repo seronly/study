10bc48c5709bf7ebca8f8333c1de87c886b13035
SerOnly <seronly@mail.ru>
2020-10-13 10:38:06 +0700

    done

--- 3sem/OOP/laba5/laba5_2.cpp
+++ 3sem/OOP/laba5/laba5_2.cpp
@@ -4,8 +4,18 @@
 #include <cassert>
 using namespace std;
 
-class Cstr
+class Calc
 {
+public:
+    float a, b;
+    void Plus()
+    {
+        cout << "a + b = " << (a + b) << endl;
+    }
+    void DelPr()
+    {
+        cout << "a /= b = " << (a /= b) << endl;
+    }
 };
 
 int main()
@@ -13,6 +23,11 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
+    Calc obj;
+    obj.a = 15;
+    obj.b = 12;
+    obj.Plus();
+    obj.DelPr();
 
     return 0;
 }
