10db495910d364410e288dab7a524d7d5ef7c073
SerOnly <seronly@mail.ru>
2020-10-20 09:05:32 +0700

    only added

--- 3sem/OOP/laba6/laba6_2.cpp
+++ 3sem/OOP/laba6/laba6_2.cpp
@@ -6,8 +6,27 @@ using namespace std;
 
 class Str
 {
-
 public:
+    string a;
+    Str operator++(); // в заглавные
+    Str operator--(); // самое короткое слово в строке
+    void show()
+    {
+        cout << a << endl;
+    };
+};
+Str Str::operator++()
+{
+    char *toUp = new char[a.length() + 1];
+    for (int i = 0; i < a.length() + 1; i++)
+    {
+        toUp[i] = toupper(a[i]);
+    }
+    cout << toUp << endl;
+};
+Str Str::operator--()
+{
+    cout << "Оператор --" << endl;
 };
 
 int main()
@@ -15,6 +34,12 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
+    Str A, B, C;
+    A.a = "Hello, world!";
+    B = A;
+    B.show();
+    ++B;
+    --B;
 
     return 0;
 }
