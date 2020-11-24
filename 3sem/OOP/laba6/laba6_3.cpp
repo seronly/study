9875b1f1c9a20d5951cf2a174a825f45c94f77c9
SerOnly <seronly@mail.ru>
2020-11-24 15:36:54 +0700

    done

--- 3sem/OOP/laba6/laba6_3.cpp
+++ 3sem/OOP/laba6/laba6_3.cpp
@@ -3,58 +3,122 @@
 #include <locale>
 #include <cassert>
 using namespace std;
+#define MAX 100
 
 class Stc
 {
+private:
+    int data[MAX];
+
 public:
-    struct E
+    int n;
+    Stc()
     {
-        int data;
-        E *next;
-        E *head;
-    };
-
-    void add(int d, E *&RR);
-    void show(E *RR);
-};
-
-void Stc::add(int dt, E *&RR)
-{
-    E *e;
-    e = new E;
-    e->data = dt;
-    e->next == RR->head;
-    RR->head = e;
-}
-
-void Stc::show(E *RR)
-{
-    E *a = RR->head;
-    while (a != NULL)
-    {
-        cout << a->data << endl;
-        a = a->next;
+        n = 0;
     }
-}
+    ~Stc() {}
+    int get_top()
+    {
+        if (n == 0)
+        {
+            cerr << "Стек пуст!" << endl;
+            return 0;
+        }
+        return data[n - 1];
+    }
+    void push(int item)
+    {
+        if (n < MAX)
+        {
+            data[n++] = item;
+        }
+        else
+        {
+            cerr << "Стек переполнен!" << endl;
+        }
+    }
+    void del()
+    {
+        if (n == 0)
+        {
+            cerr << "Стек пуст!" << endl;
+        }
+        else
+        {
+            n--;
+        }
+    }
+    int size()
+    {
+        return n;
+    }
+    void show()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << data[i] << " ";
+        }
+        cout << endl;
+    }
+};
 
 int main()
 {
     setlocale(LC_ALL, "65001");
     srand(time(NULL));
-    int n1 = 0, n2 = 0, count;
-    Stc s1, s2;
-    Stc::E *stct = new Stc::E;
-    stct->head = NULL;
-    cout << "Введите количество элементов: ";
-    cin >> count;
-    for (int i = 0; i < count; i++)
+
+    Stc s1, s2, s3;
+    int k1, k2, n1 = 0, n2 = 0;
+    cout << "Введите размер первого стека: ";
+    cin >> k1;
+    cout << "Введите размер второго стека: ";
+    cin >> k2;
+    for (int i = 0; i < k1; i++)
     {
         n1 = rand() % (n1 + 5) + n1;
-        n2 = rand() % (n2 + 5) + n2;
-        s1.add(n1, stct);
-        s2.add(n2, stct);
+        s1.push(n1);
     }
-    s1.show(stct);
-    s2.show(stct);
+    for (int i = 0; i < k1; i++)
+    {
+        n2 = rand() % (n2 + 5) + n2;
+        s2.push(n2);
+    }
+    cout << "Первый стек:" << endl;
+    s1.show();
+    cout << "Второй стек:" << endl;
+    s2.show();
+    while ((s1.size() > 0) || (s2.size() > 0))
+    {
+        if ((s1.size() > 0) && (s2.size() > 0))
+        {
+            if (s1.get_top() > s2.get_top())
+            {
+                s3.push(s1.get_top());
+                s1.del();
+            }
+            else
+            {
+                s3.push(s2.get_top());
+                s2.del();
+            }
+        }
+        else if (s1.size() > 0)
+        {
+            s3.push(s1.get_top());
+            s1.del();
+        }
+        else
+        {
+            s3.push(s2.get_top());
+            s2.del();
+        }
+    }
+    cout << "Размер 3 стека: " << s3.size() << endl;
+    cout << "Третий стек с объединением: " << endl;
+    while (s3.size())
+    {
+        cout << s3.get_top() << " ";
+        s3.del();
+    }
     return 0;
 }
