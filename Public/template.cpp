a7103998ce54c8e0a9a53f76c119fb5c21ad9b3b
SerOnly <seronly@mail.ru>
2021-03-05 09:34:19 +0700

    Add public

---
+++ Public/template.cpp
@@ -0,0 +1,19 @@
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "65001");
+    system("chcp 65001");
+    system("cls");
+
+    return 0;
+}
